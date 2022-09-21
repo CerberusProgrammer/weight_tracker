aaa382092bdfef0f5e575afb0d37545d3fd522fa
cerberus <cerberusprogrammer@gmail.com>
2022-09-20 19:27:30 -0700

    UwU

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mrx_charts/mrx_charts.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,9 +12,11 @@ class Home extends StatefulWidget {
 }
 
 class _Home extends State<Home> {
-  List<String> weights = [];
-  List<Text> texts = [const Text('data')];
+  List<Text> texts = [];
   final weight = TextEditingController();
+
+  double lowerWeight = 200;
+  double higherWeight = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +24,13 @@ class _Home extends State<Home> {
       appBar: AppBar(
         title: const Text('Weight Tracker'),
       ),
-      body: Center(
-        child: ListView(
-          children: texts,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Chart(
+          layers: layers(),
+          padding: const EdgeInsets.symmetric(horizontal: 30.0).copyWith(
+            bottom: 12.0,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -55,7 +64,35 @@ class _Home extends State<Home> {
                   actions: [
                     ElevatedButton(
                       onPressed: () {
-                        texts.add(Text(weight.toString()));
+                        if (weight.text.isEmpty) {
+                          Navigator.of(context).pop();
+                          return;
+                        }
+
+                        if (double.parse(weight.text) < 40 ||
+                            double.parse(weight.text) > 200) {
+                          Navigator.of(context).pop();
+                          return;
+                        }
+
+                        if (texts.length == 5) {
+                          texts.removeAt(0);
+                        }
+
+                        double w = double.parse(weight.text);
+
+                        if (lowerWeight > w) {
+                          lowerWeight = w;
+                        }
+
+                        if (higherWeight < w) {
+                          higherWeight = w;
+                        }
+
+                        setState(() {
+                          texts.add(Text(weight.text));
+                        });
+                        Navigator.of(context).pop();
                       },
                       child: const Text('Add'),
                     ),
@@ -67,5 +104,89 @@ class _Home extends State<Home> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  List<ChartLayer> layers() {
+    final from = DateTime(2021, 4);
+    final to = DateTime(2021, 8);
+
+    DateTime now = DateTime.now();
+
+    final frequency =
+        (to.millisecondsSinceEpoch - from.millisecondsSinceEpoch) /
+            (texts.length - 1);
+
+    return [
+      ChartHighlightLayer(
+        shape: () => ChartHighlightLineShape<ChartLineDataItem>(
+          backgroundColor: const Color.fromARGB(255, 58, 58, 58),
+          currentPos: (item) => item.currentValuePos,
+          radius: const BorderRadius.all(Radius.circular(8.0)),
+          width: 60.0,
+        ),
+      ),
+      ChartAxisLayer(
+        settings: ChartAxisSettings(
+          x: ChartAxisSettingsAxis(
+            frequency: frequency,
+            max: to.millisecondsSinceEpoch.toDouble(),
+            min: from.millisecondsSinceEpoch.toDouble(),
+            textStyle: TextStyle(
+              color: Colors.white.withOpacity(0.6),
+              fontSize: 10.0,
+            ),
+          ),
+          y: ChartAxisSettingsAxis(
+            frequency: 10.0,
+            max: 150,
+            min: 40,
+            textStyle: TextStyle(
+              color: Colors.white.withOpacity(0.6),
+              fontSize: 10.0,
+            ),
+          ),
+        ),
+        labelX: (value) =>
+            DateTime(now.year, now.month, now.day).toString().substring(0, 10),
+        labelY: (value) => value.toInt().toString(),
+      ),
+      ChartLineLayer(
+        items: List.generate(
+          texts.length,
+          (index) => ChartLineDataItem(
+            x: (index * frequency) + from.millisecondsSinceEpoch,
+            value: double.parse(texts[index].data.toString()),
+          ),
+        ),
+        settings: const ChartLineSettings(
+          color: Color(0xFF8043F9),
+          thickness: 4.0,
+        ),
+      ),
+      ChartTooltipLayer(
+        shape: () => ChartTooltipLineShape<ChartLineDataItem>(
+          backgroundColor: Colors.white,
+          circleBackgroundColor: Colors.white,
+          circleBorderColor: const Color(0xFF331B6D),
+          circleSize: 4.0,
+          circleBorderThickness: 2.0,
+          currentPos: (item) => item.currentValuePos,
+          onTextValue: (item) =>
+              '${item.value.toString()} kg\n${DateTime(now.year, now.month, now.day).toString().substring(0, 10)}',
+          marginBottom: 6.0,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 8.0,
+          ),
+          radius: 6.0,
+          textStyle: const TextStyle(
+            color: Color(0xFF8043F9),
+            letterSpacing: 0.2,
+            fontSize: 14.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    ];
   }
 }
