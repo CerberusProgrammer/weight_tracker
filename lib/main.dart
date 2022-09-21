4ef08b46405fc1ab50d02f6b3f10d2f720ac30c9
cerberus <cerberusprogrammer@gmail.com>
2022-09-20 19:27:55 -0700

    uwu?

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'home.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: 'Weight Tracker',
-    theme: ThemeData.dark(),
+    theme: ThemeData.dark(useMaterial3: true),
     home: const Home(),
   ));
 }
